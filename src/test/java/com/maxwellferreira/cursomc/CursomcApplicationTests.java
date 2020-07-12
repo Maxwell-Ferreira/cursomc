e5cbf3e18ffc566cdb89ddab36156ffaf25d46aa
Maxwell-Ferreira <ferreiramaxwell531@gmail.com>
2020-07-12 09:21:43 -0300

    Projeto iniciado

---
+++ src/test/java/com/maxwellferreira/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxwellferreira.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
